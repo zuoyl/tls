16a761c23de1ed48c38bd33bb9c53101210aa6bf
jenson <jenson.zuo@gmail.com>
2013-02-23 21:18:39 +0800

    add files to support exception from ASTBuilder

--- tlang/Include/Runtime/Exception.h
+++ tlang/Include/Runtime/Exception.h
@@ -6,27 +6,49 @@
 #ifndef TCC_EXCEPTION_H
 #define TCC_EXCEPTION_H
 
-#include <Common.h>
+#include "Common.h"
 #include <stdexcept>
 
-namespace Exception {
-    class MccException : public std::exception {
-    public:
-        virtual const char* what() const throw() = 0;
-        virtual void print(iostream &ios) throw() = 0;
-    };
+class TccException : public std::exception {
+public:
+    virtual const char* what() const throw() = 0;
+    virtual void print(iostream &ios) throw() = 0;
+};
     
-    class NoMatchedToken : public MccException {
-    public:
-        NoMatchedToken(){}
-        NoMatchedToken(const string &token):m_token(token){}
-        ~NoMatchedToken() throw() {}
-        void print(iostream &ios) throw(){}
-        char* what()const throw() { return NULL; }
-    protected:
-        string m_token;
-    };
-} // namespace 
+class NoMatchedToken : public MccException {
+public:
+    NoMatchedToken(){}
+    NoMatchedToken(const string &token):m_token(token){}
+    ~NoMatchedToken() throw() {}
+    void print(iostream &ios) throw(){}
+    char* what()const throw() { return NULL; }
+protected:
+    string m_token;
+};
+
+
+class InvalidExpression : public TccException {
+public:
+    InvalidExpression(){}
+    InvalidExpression(const string &expr):m_item(expr){}
+    ~InvalidExpression() throw() {}
+    void print(iostream &ios) throw(){}
+    char* what()const throw() { return m_item.c_str(); }
+private:
+    string m_item;
+
+};
+
+class InvalidStatement : public TccExcetpion {
+public:  
+    InvalidStatement(){}
+    InvalidStatement(const string &stmt):m_item(stmt){}
+    ~InvalidStatement() throw() {}
+    void print(iostream &ios) throw(){}
+    char* what()const throw() { return m_item.c_str(); }
+private:
+    string m_item;
+};
 
 
 #endif // TCC_EXCEPTION_H
