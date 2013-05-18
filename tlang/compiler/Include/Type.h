6edb901bbd9258f44eaa36e1a09ab54772d508ea
jenson <jenson.zuo@gmail.com>
2013-05-18 11:56:12 +0800

    add implementations to Type

--- tlang/compiler/Include/Type.h
+++ tlang/compiler/Include/Type.h
@@ -39,43 +39,38 @@ public:
     //! constructor
     Type(){} 
     Type(const string name, bool isPublic)
-    {
-        m_name = name;
-        m_isPublic = isPublic;
-    }
+        :m_name(name), m_isPublic(isPublic){}
 
     //! destructor
-    virtual ~Type(){}
+    virtual ~Type();
     
     //! setter/getter for the type publicity
     virtual void setPublic(bool isPublic) { m_isPublic = isPublic;  }
     virtual bool isPublic() const { return m_isPublic;  }
     
-    //! set type name
+    //! setter/getter for type name
     virtual void setName(const string &name) { m_name = name; }
-    
-    // !get type name
-    virtual const string& getName() const  { return m_name; }
+    virtual const string& getName() { return m_name; }
     
     //! get the type's size
     virtual int getSize() { return m_size; }
     
     //! getter/setter for slot type member in current type
-    virtual void addSlot(const string &name, Type *slot) { }
-    virtual Type* getSlot(const string &name) const { return NULL; }
+    virtual void addSlot(const string &name, Type *slot);
+    virtual Type* getSlot(const string &name);
         
     //! get slot by index
-    virtual int getSlotCount() const { return 0; }
-    virtual Type* getSlot(int index) { return NULL;} 
+    virtual int getSlotCount();
+    virtual Type* getSlot(int index);
     
     //! wether the type is compatible with other type 
-    virtual bool isCompatibleWithType(Type *type) { return false; }
+    virtual bool isCompatibleWithType(Type &type) { return false; }
    
     //! wether the type is equal with specifier type
-    virtual bool operator ==(Type *type) { return false; }
+    virtual bool operator ==(Type &type) { return false; }
     
     //! type assign
-    virtual Type& operator =(Type *type){ return *this; }
+    virtual Type& operator =(Type &type){ return *this; }
    
     //! all type should support virtual table
     virtual bool hasVirtualTable() { return false; }
@@ -89,6 +84,8 @@ protected:
     bool m_isPublic;
     string m_name;
     int m_size;
+    map<string, Type *> m_slots;
+    vector<Type *> m_slotseqs;
     
 };
 // class TypeDomain - which contalls all type
--- tlang/compiler/Type/Type.cpp
+++ tlang/compiler/Type/Type.cpp
@@ -5,6 +5,51 @@
 
 #include "Type.h"
 
+/// Type implementation
+Type::~Type()
+{
+    // free all slots
+    map<string, Type*>::iterator ite = m_slots.begin();
+    for (; ite != m_slots.end(); ite++)
+        delete ite->second;
+    m_slots.clear();
+    m_slotseqs.clear();
+    m_isPublic = false;
+}
+
+void Type::addSlot(const string &name, Type *type)
+{
+    if (!name.empty() && type) {
+        if (m_slots.find(name) != m_slots.end()) {
+            m_slots.insert(make_pair(name, type));
+            m_slotseqs.push_back(type);
+        }
+    }
+}
+
+Type* Type::getSlot(const string &name)
+{
+    if (m_slots.find(name) != m_slots.end())
+        return m_slots[name];
+    else
+        return NULL;
+}
+
+int Type::getSlotCount()
+{
+    assert(m_slots.size() == m_slotseqs.size());
+    return (int)m_slots.size();
+}
+
+Type* Type::getSlot(int index)
+{
+    if (index >= 0 && index < (int)m_slots.size())
+        return m_slotseqs[index];
+    else
+        return NULL;
+}
+
+
 // TypeDomain implement
 
 TypeDomain::TypeDomain()
